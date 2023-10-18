3b036fa6c0cce8cf49e61a6adccaf8f10401f4b3
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-10-19 01:38:31 +0600

    implemented unique numbers and implemented sorting condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Ball from './Ball/ball'
-import './App.css'
-import { useState } from 'react'
+import Ball from './Ball/Ball';
+import './App.css';
+import {useState} from 'react';
 
 function App() {
   const [balls, setBalls] = useState([
@@ -9,34 +9,45 @@ function App() {
     {value: 16},
     {value: 23},
     {value: 32},
-  ])
+  ]);
+
+  const shuffle = (array:number[]) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
 
   const changeValues = () => {
+    let arrayNumbers: number[] = [];
+
+    for (let i = 5; i <= 36; i++) {
+      arrayNumbers.push(i);
+    }
+
+    let newNumbers = shuffle(arrayNumbers).slice(0, 5).sort((a:number, b:number) => a - b);
+
     setBalls([
-      {value: randomValue()},
-      {value: randomValue()},
-      {value: randomValue()},
-      {value: randomValue()},
-      {value: randomValue()},
-    ])
-  }
+      {value: newNumbers[0]},
+      {value: newNumbers[1]},
+      {value: newNumbers[2]},
+      {value: newNumbers[3]},
+      {value: newNumbers[4]},
+    ]);
 
-  const randomValue = () => {
-    return Math.floor(Math.random() * (36 - 5 + 1)) + 5;
   }
-
+  
   return (
-      <div className='App'>
-        <button type='button' onClick={changeValues}>New numbers</button>
-        <div className="ball-wrapper">
-          <Ball value={balls[0].value}/>
-          <Ball value={balls[1].value}/>
-          <Ball value={balls[2].value}/>
-          <Ball value={balls[3].value}/>
-          <Ball value={balls[4].value}/>
-        </div>
+    <div className='App'>
+      <button type='button' onClick={changeValues}>
+        New numbers
+      </button>
+      <div className='ball-wrapper'>
+        <Ball value={balls[0].value} />
+        <Ball value={balls[1].value} />
+        <Ball value={balls[2].value} />
+        <Ball value={balls[3].value} />
+        <Ball value={balls[4].value} />
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
