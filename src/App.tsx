c5e815745e919e68a2c21bcf37f0172fa7bf07d5
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-10-19 00:37:55 +0600

    Created initial state, added setState, and created random value function

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,37 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
+  const [balls, setBalls] = useState([
+    {value: 5},
+    {value: 11},
+    {value: 16},
+    {value: 23},
+    {value: 32},
+  ])
+
+  const changeValues = () => {
+    setBalls([
+      {value: randomValue()},
+      {value: randomValue()},
+      {value: randomValue()},
+      {value: randomValue()},
+      {value: randomValue()},
+    ])
+  }
+
+  const randomValue = () => {
+    return Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+  }
+
   return (
       <div className='App'>
-        <button type='button'>New numbers</button>
+        <button type='button' onClick={changeValues}>New numbers</button>
         <div className="ball-wrapper">
-          <Ball value={5}/>
-          <Ball value={11}/>
-          <Ball value={16}/>
-          <Ball value={23}/>
-          <Ball value={32}/>
+          <Ball value={balls[0].value}/>
+          <Ball value={balls[1].value}/>
+          <Ball value={balls[2].value}/>
+          <Ball value={balls[3].value}/>
+          <Ball value={balls[4].value}/>
         </div>
       </div>
   )
